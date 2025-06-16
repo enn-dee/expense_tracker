318cec262d79b4f47ccb775e029396173e25dd7a
enn-dee <nddar191131@gmail.com>
2025-06-16 20:34:15 +0530

    feat: update dependencies and add Prisma integration
    - Added @clerk/nextjs and @prisma/client to dependencies in package.json
    - Updated package-lock.json with new dependencies and their versions
    - Removed unused SVG files from the public directory
    - Created db.ts file for Prisma client initialization
    - Added migration files for User and Transaction tables in the database
    - Updated Prisma schema to define User and Transaction models

---
+++ lib/db.ts
@@ -0,0 +1,14 @@
+import { PrismaClient } from "@/app/generated/prisma";
+
+declare global {
+    var prisma: PrismaClient | undefined;
+    var nadeem : "nadeem ahmad"
+}
+
+export const db = globalThis.prisma || new PrismaClient()
+
+if (process.env.NODE_ENV !== "production") {
+    globalThis.prisma = db;
+}
+
+
